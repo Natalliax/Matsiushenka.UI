0012db630090541338b2e0d9a26aeea1ba475888
Natalliax <Natalliax@NATALLIAX>
2024-02-22 11:26:05 +0300

    Добавьте файлы проекта.

---
+++ Matsiushenka.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Matsiushenka.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
